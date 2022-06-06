36f19e57207b0786f9c198e733ae197022c66ca8
Tom Mills <t.mills124@gmail.com>
2022-06-06 21:42:21 +0100

    wrote testing script & solved 3rd kata

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,8 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number => {
+    const splitNo = number.toString().split("");
+    const realDigits = splitNo.map(Number);
+    realDigits.reverse();
+    return realDigits;    
+};
 
 module.exports = numberToReversedDigits;
--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,5 +1,10 @@
 const { numberToReversedDigits } = require("../src");
 
 describe("numberToReversedDigits", () => {
-  test("returns a reversed array of the number's digits", () => {});
+  test("returns a reversed array of the number's digits", () => {
+    expect(numberToReversedDigits(12345)).toEqual([5,4,3,2,1]);
+    expect(numberToReversedDigits(42879)).toEqual([9,7,8,2,4]);
+    expect(numberToReversedDigits(42)).toEqual([2,4]);
+    expect(numberToReversedDigits(789)).toEqual([9,8,7]);
+    });
 });
