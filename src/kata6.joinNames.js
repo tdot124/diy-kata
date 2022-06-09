8ed1c297ef4e1640aaa7967adcf0f34f8cdcd351
Tom Mills <t.mills124@gmail.com>
2022-06-09 22:28:47 +0100

    wrote tests & solved 6th Kata joinNames

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,19 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+    
+    if (namesObj.length > 2){
+    
+    const start = namesObj.shift().name;
+    const end = ' & '+ namesObj.pop().name;
+    const middleArr = namesObj.map (obj => ', ' + obj.name);
+    const middle = middleArr.reduce((acc,person) => acc + person);    
+    return `${start + middle + end}`;
+    
+    } else if (namesObj.length === 2) {
+        return `${namesObj.shift().name} & ${namesObj.pop().name}`;
+        } else {
+            return `${namesObj.shift().name}`;
+        }
+
+};
 
 module.exports = joinNames;
