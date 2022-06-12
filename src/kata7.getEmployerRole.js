64a0c8a1be51109e211f79048b1aa4bea2e3abcf
Tom Mills <t.mills124@gmail.com>
2022-06-12 15:18:03 +0100

    set tests & solved 7th DIY kata

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -1,3 +1,7 @@
-const getEmployerRole = (employeeName, employees) => {};
+const getEmployerRole = (employeeName, employees) => {
+const result = employees.find(person => person.name === employeeName);
+return result.role;
+
+};
 
 module.exports = getEmployerRole;
